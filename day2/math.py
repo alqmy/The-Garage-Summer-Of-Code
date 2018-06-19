78776027a2ae5e7c6bfd7c31664405c312dece9f
Victor Martinez <victor@alqmy.io>
2018-06-19 13:06:05 -0400

    updated math

--- day2/math.py
+++ day2/math.py
@@ -4,12 +4,20 @@ c = 0
 
 # addition in python
 c = a + b
+print(c)
 
 # subtraction in python
 c = a - b
+print(c)
 
 # multiplication in python
 c = a * b
+print(c)
 
 # division in python 
 c = a / b # a divided by b
+print(c)
+
+# exponent
+c = a ** b # a^b
+print(c)
